e8c4e419e6437e8ad64cb67ab8332c323db3d72e
labsfiap <pedrocastrosilva50@gmail.com>
2026-05-06 20:44:46 -0300

    Aula 6 de maio

---
+++ src/AulaMai05/TechGeo/Forma/Forma.java
@@ -0,0 +1,5 @@
+package AulaMai05.TechGeo.Forma;
+
+public abstract class Forma {
+    public abstract double calcularArea();
+}
---
+++ src/AulaMai05/TechGeo/Forma/Volume.java
@@ -0,0 +1,5 @@
+package AulaMai05.TechGeo.Forma;
+
+public interface  Volume {
+    public double calcularVolume();
+}
